f15b33c46c7386e5d91c1f59ee532fd3128743c6
Vitor Abrantes Valle <vitor.valle@alunos.unicesumar.edu.br>
2023-11-07 19:06:32 -0300

    arrumando as imagens do carrossel

--- src/containers/categories/imageByIndex.ts
+++ src/containers/categories/imageByIndex.ts
@@ -1,9 +1,11 @@
-import imageCoupe from "../../images/coupeImage.jpg"
-import imageHatch from '../../images/hatchImage.jpg'
-import imageSedan from '../../images/sedanImage.jpg'
-import imageWagon from '../../images/wagonimage.jpg'
-
-export const images = [imageCoupe, imageHatch, imageSedan, imageWagon]
+import imageCoupe from "../../images/Coupes.jpg"
+import imageHatch from '../../images/Hatches.jpg'
+import imageSedan from '../../images/Sedans.jpg'
+import imageWagon from '../../images/Peruas.jpg'
+import imagePickup from '../../images/Picapes.jpg'
+import imageSuv from '../../images/Suv.jpg'
+import imageZero from '../../images/Zero.jpg'
+export const images = [imageCoupe, imageHatch, imageSedan, imageWagon, imagePickup, imageSuv, imageZero]
 
 const imageByIndex = (index: number) => images[index % images.length]
 
