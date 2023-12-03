76e642160e9b707376f0b1f8a3da7d720de8f7cb
Ostetti <felipeostettiferi@hotmail.com>
2023-12-03 19:39:28 -0300

    update quase lá

--- src/app/api/usuarios/route.ts
+++ src/app/api/usuarios/route.ts
@@ -5,23 +5,58 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { filtrar } from '@/components/searchBox/searchBox';
 import Usuario from '@/models/usuarios';
 
-export async function POST(req: Request) {
+export async function POST(req: any, res: any) {
     const {nome, email, senha} =  await req.json();
     await connectDb();
     await Usuario.create({nome, email, senha})
     return NextResponse.json({message: "Usuario cadastrado"}, {status: 201});
+
+    if (!Usuario) {
+        res.status(404).json({message: "Serviço não encontrado"});
+        return;
+    }
 }
 
-export async function GET(){
+export async function GET(res: any){
     await connectDb();
     const usuarios = await Usuario.find();
     return NextResponse.json({usuarios});
+
+    if (!Usuario) {
+        res.status(404).json({message: "Nenhum usuário encontrado"});
+        return;
+    }
+
 }
 
-export async function DELETE( req: any){
+export async function DELETE( req: any, res: any){
     const id = req.url.split("?")[1];
     console.log(id);
     await connectDb();
     await Usuario.findByIdAndDelete(id);
     return NextResponse.json({message: "Usuario deletado"}, {status: 200});
+
+    if (!Usuario) {
+        res.status(404).json({message: "usuário não encontrado"});
+        return;
+    }
+}
+
+export async function UPDATE(req: any, res: any){
+    const id = req.params.id;
+
+    const service = {
+        nome: req.nome,
+        email: req.email,
+        senha: req.senha,
+    };
+
+    const updateService = await Usuario.findByIdAndUpdate(id, service);
+
+    if (!Usuario) {
+        res.status(404).json({message: "usuário não encontrado"});
+        return;
+    }
+
+    res.status(200).json({service, msg: "Usuário atualziado"})
 }
